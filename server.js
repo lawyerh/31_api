79c2a5525cc75c191c15f57a97a99543aca9421c
lawyerh <lawyerh_01@yahoo.com>
2018-04-26 17:20:10 -0700

    changes to server file

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ const app = express()
 const bp = require("body-parser")
 const path = require("path")
 
+app.use(bp.json())
 
-app.use(express.static(path.join(__dirname + '/client/dist')))
-
+app.get('/', function(req, res){
+    var success = {"success": "Welcome to the card api! Search for a card by using /img/{first character of suit} + {card value}. Example: /img/c5 for five of clubs or /img/d1 for ace of diamonds."}
+    res.json(success)
+})
 
 app.get('/img/:id', function(req, res){
     let img = __dirname + "/img/" + req.params.id + ".png"
     res.sendFile(img)
 })
 
-app.use(bp.json())
 
 app.listen(8000, function(){
     console.log("Listening on 8000")
